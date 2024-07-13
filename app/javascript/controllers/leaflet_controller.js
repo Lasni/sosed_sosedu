8794da2a97ab6a1f11f379fd839ae6aa17d6cf9d
Lasni <grega.lasnibat@gmail.com>
2024-07-13 14:18:40 +0200

    learning leaflet maps, refactored code, moved map to users/show for when creating a new post.

--- app/javascript/controllers/leaflet_controller.js
+++ app/javascript/controllers/leaflet_controller.js
@@ -2,77 +2,92 @@ import { Controller } from "@hotwired/stimulus"
 import L from 'leaflet'
 import "leaflet-css"
 
-const options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
-}
-
 export default class extends Controller {
   static targets = ["mapDiv"]
 
   connect() {
     console.log('leaflet controller connect')
-    this.constructLeafletMap();
+    let map;
+    let marker;
+
+    // extract data from rais
+    const { latitude, longitude, posterUsername, ...rest } = this.mapDivTarget.dataset;
+
+    // map options
+    const mapOptions = {
+    }
+
+    // marker options
+    const markerOptions = {
+      title: posterUsername,
+      draggable: true
+    }
+
+    // construct the map
+    map = this.constructLeafletMap(parseFloat(latitude), parseFloat(longitude), 15, mapOptions);
+
+    // construct the marker
+    marker = this.constructLeafletMarker(latitude, longitude, markerOptions)
+
+    // add marker to the map
+    marker.addTo(map)
   }
 
-  constructLeafletMap() {
-    if (!this.map) {    
+  constructLeafletMap(latitude, longitude, zoom, mapOptions) {
       // Initialize Leaflet map
-      this.map = L.map(this.mapDivTarget).setView([46.0771083, 14.4801205], 15);
+      const map = L.map(this.mapDivTarget).setView([latitude, longitude], zoom, mapOptions);
 
       // Add a tile layer (you can choose different tile providers or use custom maps)
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(this.map);
-
-      // Add marker layer for displaying current location
-      this.marker = L.marker([46.0771083, 14.4801205]).addTo(this.map);
-
-      // Call geolocation once map is initialized
-      this.getCurrentPosition();
-
-      console.log('map', this.map)
-    }
+      }).addTo(map);
+      return map
   }
 
-  getCurrentPosition() {
-    navigator.geolocation.getCurrentPosition(
-      pos => this.success(pos),
-      err => this.error(err),
-      options
-    );
+  constructLeafletMarker(latitude, longitude, markerOptions) {
+    // Add marker layer for displaying current location
+    const marker = L.marker([latitude, longitude], markerOptions)
+    return marker
   }
 
-  success = (pos) => {
-    const { latitude, longitude } = pos.coords;
-    this.reverseGeocode(latitude, longitude);
-  }
 
-  error = (err) => {
-    console.error(`Geolocation error: ${err.message}`);
-  }
+  // getCurrentPosition() {
+  //   navigator.geolocation.getCurrentPosition(
+  //     pos => this.success(pos),
+  //     err => this.error(err),
+  //     options
+  //   );
+  // }
 
-  async reverseGeocode(latitude, longitude) {
-    const url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`;
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const address = data.display_name;
-      console.log(`Address: ${address}`);
-      this.showAddressOnMap(address, latitude, longitude);
-    } catch (error) {
-      console.error('Error during reverse geocoding:', error);
-    }
-  }
+  // success = (pos) => {
+  //   const { latitude, longitude } = pos.coords;
+  //   this.reverseGeocode(latitude, longitude);
+  // }
 
-  showAddressOnMap(address, latitude, longitude) {
-    if (this.marker) {
-      this.marker.setLatLng([latitude, longitude]);
-      this.marker.bindPopup(address).openPopup();
-    } else {
-      this.marker = L.marker([latitude, longitude]).addTo(this.map);
-      this.marker.bindPopup(address).openPopup();
-    }
-  }
+  // error = (err) => {
+  //   console.error(`Geolocation error: ${err.message}`);
+  // }
+
+  // async reverseGeocode(latitude, longitude) {
+  //   const url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`;
+  //   try {
+  //     const response = await fetch(url);
+  //     const data = await response.json();
+  //     const address = data.display_name;
+  //     console.log(`Address: ${address}`);
+  //     this.showAddressOnMap(address, latitude, longitude);
+  //   } catch (error) {
+  //     console.error('Error during reverse geocoding:', error);
+  //   }
+  // }
+
+  // showAddressOnMap(address, latitude, longitude) {
+  //   if (this.marker) {
+  //     this.marker.setLatLng([latitude, longitude]);
+  //     this.marker.bindPopup(address).openPopup();
+  //   } else {
+  //     this.marker = L.marker([latitude, longitude]).addTo(this.map);
+  //     this.marker.bindPopup(address).openPopup();
+  //   }
+  // }
 }
